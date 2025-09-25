d3800a7d65981d3d350924079f797eade4b3dceb
Y23MSWD31545 <harshavardhan8719@gmail.com>
2025-09-25 17:36:50 +0530

    Updetes

--- backend/digitalwallet_backend/src/main/java/com/digitalwallet/backend/DigitalwalletBackendApplication.java
+++ backend/digitalwallet_backend/src/main/java/com/digitalwallet/backend/DigitalwalletBackendApplication.java
@@ -2,10 +2,10 @@ package com.digitalwallet.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class DigitalwalletBackendApplication extends SpringBootServletInitializer{
+public class DigitalwalletBackendApplication {
   public static void main(String[] args) {
     SpringApplication.run(DigitalwalletBackendApplication.class, args);
     System.out.println("Digital Wallet  API is Running");
